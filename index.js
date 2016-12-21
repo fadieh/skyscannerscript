4a4ef2bd447aafb4ca8d5a95b618ccd017341555
Fadie Hannona <fadiehannona@gmail.com>
2016-12-21 18:44:46 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const osmosis = require("osmosis");
+
+// Time constants
+
+const TIME_MS = 1
+const TIME_SEC = TIME_MS * 1000
+const TIME_MIN = TIME_SEC * 60
+const TIME_HOUR = TIME_MIN * 60
+
+console.log(TIME_MS);
+console.log(TIME_SEC);
+console.log(TIME_MIN);
+console.log(TIME_HOUR);
